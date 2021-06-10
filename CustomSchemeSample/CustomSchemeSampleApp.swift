87b8960a4b7365af14f09e2adac350996522f362
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-10 10:46:34 +0900

    Initial Commit

---
+++ CustomSchemeSample/CustomSchemeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSchemeSampleApp.swift
+//  CustomSchemeSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomSchemeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
